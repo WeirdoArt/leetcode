437f7507eddcba506087b0afc7114cea7c228215
ナ众男神 <35830502@qq.com>
2020-05-12 07:09:12 +0800

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -13,3 +13,27 @@ public:
         return ret;
     }
 };
+
+// 以下解决方式会超出时间限制(Time Limit Exceeded)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ret;
+        
+        int i = 0, j = 0;
+        while(i < nums.size()) {
+            j = i + 1;
+            if(nums[i] + nums[j] == target && i != j) {
+                ret.push_back(i);
+                ret.push_back(j);
+                break;
+            }
+            else {
+                ++j;
+                --i;
+            }
+            ++i;
+        }
+        return ret;
+    }
+};
