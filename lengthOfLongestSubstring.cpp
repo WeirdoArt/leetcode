97d98c4ce461fba2f3bfce58da3f81b21be7b34b
ナ众男神 <35830502@qq.com>
2020-05-13 12:54:46 +0800

    Update lengthOfLongestSubstring.cpp

--- lengthOfLongestSubstring.cpp
+++ lengthOfLongestSubstring.cpp
@@ -1,4 +1,4 @@
-// Brute Force
+// Approach 1: Brute Force
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -25,5 +25,61 @@ public:
             s.insert(ch);
         }
         return true;
+    }
+};
+
+
+// Approach 2: Sliding Window
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        set<char>hashset;
+        int ans = 0, i = 0, j = 0;
+        while(i < n && j < n) {
+            if(hashset.find(s[j]) == hashset.end()) {
+                hashset.insert(s[j++]);
+                ans = ans > j - i ? ans : j - i;
+            }
+            else {
+                hashset.erase(s[i++]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Approach 3: Sliding Window Optimized
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        map<char, int>hashmap;
+        int ans = 0;
+        for(int i = 0, j = 0; j < n; j++) { 
+            auto tmp = hashmap.find(s[j]);
+            if(tmp != hashmap.end()) {
+                i = max(tmp->second, i); // 1
+            }
+            ans = max(ans, j - i + 1);
+            hashmap[s[j]] = j + 1;
+        }
+        return ans;
+    }
+};
+
+// replace the Map with an integer array as direct access table.
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int ans = 0;
+        int index[128] = {0};
+        for(int i = 0, j = 0; j < n; j++) { // n=7
+            i = max(index[s[j]], i);
+            ans = max(ans, j - i + 1);
+            index[s[j]] = j + 1;
+        }
+        return ans;
     }
 };
