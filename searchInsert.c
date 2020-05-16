087c3d675adce8e0b146200bdbaee4750b3dd565
ナ众男神 <35830502@qq.com>
2020-05-17 03:13:02 +0800

    Create searchInsert.c

---
+++ searchInsert.c
@@ -0,0 +1,22 @@
+
+
+
+
+int searchInsert(int* nums, int numsSize, int target){
+    if(!nums || numsSize <= 0) {
+        return 0;
+    }
+    
+    for(int i = 0; i < numsSize; ++i) {
+        if(nums[i] < target) {
+            continue;
+        }
+        if(nums[i] == target) {
+            return i;
+        }
+        else if(nums[i] > target) {
+            return i;
+        }
+    }
+    return numsSize;
+}
