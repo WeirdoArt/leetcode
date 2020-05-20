63929317e53d5d5b403a13d9c510d80f30464ed4
ナ众男神 <35830502@qq.com>
2020-05-20 17:02:20 +0800

    Update isValid.cpp

--- isValid.cpp
+++ isValid.cpp
@@ -3,7 +3,6 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stack;
-
         if (s.length() == 0) {
             return true;
         }
@@ -14,7 +13,7 @@ public:
         for (int i = 0; i < s.length(); i++) {
             char c = s[i];
             if(c == '}' || c == ')' || c == ']') {
-                if (stack.size() == 0) return false;
+                if (stack.empty()) return false;
                 if (c == '}' && stack.top() != '{') return false;
                 if (c == ')' && stack.top() != '(') return false;
                 if (c == ']' && stack.top() != '[') return false;
@@ -24,12 +23,18 @@ public:
                 stack.push(c);
             }
         }
-        if(stack.size() == 0) return true;
+        if(stack.empty()) {
+            return true;
+        }
         return false;
     }
 };
 /**
-
+解题思路：
+    关键点：利用栈的特性
+    既然是找有效的匹配，那肯定得找到“对应”的右括号才算有效，所以在迭代过程中如果是左括号就都压栈，
+    同时在迭代过程中如果遇到“任何”一个右括号就去看是否与栈顶元素相“对应”，如果为真就弹栈否则直接返回false，
+    最后检查栈是否为空，如果为空表示全部匹配返回true，否则就是有左括号未匹配到对应的右括号返回false；
  */
 
 // Approach 2: Stacks && map
@@ -53,7 +58,7 @@ public:
         for (int i = 0; i < s.length(); i++) {
             char c = s[i];
             if(map.find(c) != map.end()) {
-                if (stack.size() == 0 || stack.top() != map.find(c)->second) {
+                if (stack.empty() || stack.top() != map.find(c)->second) {
                     return false;
                 }
                 stack.pop();
@@ -62,9 +67,13 @@ public:
                 stack.push(c);
             }
         }
-        if(stack.size() == 0) {
+        if(stack.empty()) {
             return true;
         }
         return false;
     }
 };
+/**
+解题思路：
+    此方法与以上方法思路一样，只不过加入了map方便操作，代码也更易读。
+ */
