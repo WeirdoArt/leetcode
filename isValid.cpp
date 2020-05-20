98907c60d3b3485d7242b12212f4146795779ebf
ナ众男神 <35830502@qq.com>
2020-05-20 16:41:23 +0800

    Update isValid.cpp

--- isValid.cpp
+++ isValid.cpp
@@ -1,31 +1,70 @@
+// Approach 1: Stacks
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> stack;
 
-        if(s.length() == 0){
+        if (s.length() == 0) {
             return true;
         }
-        if(s.length() == 1){
+        if (s.length() == 1) {
             return false;
         }
 
-        for(int i =0 ; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s[i];
-            cout << c;
-            if(c == '}'|| c == ')' || c == ']'){
-                if(stack.size() == 0 ) return false;
-                if(c == '}' && stack.top() != '{') return false;
-                if(c == ')' && stack.top() != '(') return false;
-                if(c == ']' && stack.top() != '[') return false;
+            if(c == '}' || c == ')' || c == ']') {
+                if (stack.size() == 0) return false;
+                if (c == '}' && stack.top() != '{') return false;
+                if (c == ')' && stack.top() != '(') return false;
+                if (c == ']' && stack.top() != '[') return false;
                 stack.pop();
-
-             } else {
+            } 
+            else {
                 stack.push(c);
             }
         }
-        cout << "reached end";
         if(stack.size() == 0) return true;
+        return false;
+    }
+};
+/**
+
+ */
+
+// Approach 2: Stacks && map
+class Solution {
+public:
+    bool isValid(string s) {
+        map<char, char> map({
+            pair<char, char>(')', '('), 
+            pair<char, char>('}', '{'), 
+            pair<char, char>(']', '[')
+        });
+        stack<char> stack;
+
+        if (s.length() == 0) {
+            return true;
+        }
+        if (s.length() == 1) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s[i];
+            if(map.find(c) != map.end()) {
+                if (stack.size() == 0 || stack.top() != map.find(c)->second) {
+                    return false;
+                }
+                stack.pop();
+            } 
+            else {
+                stack.push(c);
+            }
+        }
+        if(stack.size() == 0) {
+            return true;
+        }
         return false;
     }
 };
