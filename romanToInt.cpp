f5f76c68fbc3dfbb75c193aafed653a6884341e9
ナ众男神 <35830502@qq.com>
2020-05-21 18:45:07 +0800

    Update romanToInt.cpp

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -13,7 +13,7 @@ public:
         });
     
         int res = 0, prev = 0;
-        //for (int i = s.length() - 1; i >= 0; --i) {
+        for (int i = s.length() - 1; i >= 0; --i) {
             auto curr = map.find(s[i]);
             if (curr != map.end()) {
                 if (curr->second >= prev) {
