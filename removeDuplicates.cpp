40556848a724c80b657322b17e69bc60fccfa8ce
ナ众男神 <35830502@qq.com>
2020-05-23 23:15:39 +0800

    Update removeDuplicates.cpp

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,18 +1,20 @@
+// Approach 1: Two Pointers
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() == 0) {
+        if (nums.size() == 0) {
             return 0;
         }
         
-        for (vector<int>::iterator iter = nums.begin(); 
-             iter != nums.end(), iter + 1 != nums.end(); ++iter) {
+        for (auto iter = nums.begin(); iter + 1 != nums.end(); ++iter) {
             if (*iter == *(iter + 1)) {
-                nums.erase(iter + 1);
-                iter--;
+                nums.erase(iter--);
             }
-            
         }
         return nums.size();
     }
 };
+/**
+解题思路：
+    这道题的关键是不让用额外的空间，C++会很简单，每次迭代观察相邻两个元素就可以了，C的话会很麻烦还要移动剩余元素
+ */
